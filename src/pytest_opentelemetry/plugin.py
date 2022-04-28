9c1475a09fa273f8051054fb168934fdc99b1fc8
Chris Guidry <chris@theguidrys.us>
2022-04-28 10:08:54 -0400

    Adding some help text on the command line parameters.

--- src/pytest_opentelemetry/plugin.py
+++ src/pytest_opentelemetry/plugin.py
@@ -3,8 +3,29 @@ from _pytest.config.argparsing import Parser
 
 
 def pytest_addoption(parser: Parser) -> None:
-    parser.addoption("--export-traces", action="store_true", default=False)
-    parser.addoption("--trace-parent", action="store", default=None)
+    group = parser.getgroup('pytest-opentelemetry', 'OpenTelemetry for test runs')
+    group.addoption(
+        "--export-traces",
+        action="store_true",
+        default=False,
+        help=(
+            'Enables exporting of OpenTelemetry traces via OTLP, by default to '
+            'http://localhost:4317.  Set the OTEL_EXPORTER_OTLP_ENDPOINT environment '
+            'variable to specify an alternative endpoint.'
+        ),
+    )
+    group.addoption(
+        "--trace-parent",
+        action="store",
+        default=None,
+        help=(
+            'Specify a trace parent for this pytest run, in the form of a W3C '
+            'traceparent header, like '
+            '00-1234567890abcdef1234567890abcdef-fedcba0987654321-01.  If a trace '
+            'parent is provided, this test run will appear as a span within that '
+            'trace.  If it is omitted, this test run will start a new trace.'
+        ),
+    )
 
 
 def pytest_configure(config: Config) -> None:
