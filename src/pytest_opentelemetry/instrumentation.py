7396174f7c8891522a88fd56c155a501ffdab363
Chris Guidry <chris@theguidrys.us>
2022-05-03 07:53:51 -0400

    Switching to the opentelemetry-container-distro.

--- src/pytest_opentelemetry/instrumentation.py
+++ src/pytest_opentelemetry/instrumentation.py
@@ -1,6 +1,5 @@
 from typing import Any, Generator, Optional
 
-import opentelemetry.sdk.trace as trace_sdk
 import pytest
 from _pytest.config import Config
 from _pytest.main import Session
@@ -9,15 +8,14 @@ from _pytest.reports import TestReport
 from _pytest.runner import CallInfo
 from opentelemetry import propagate, trace
 from opentelemetry.context.context import Context
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.resources import Resource, get_aggregated_resources
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.semconv.trace import SpanAttributes
-from opentelemetry.trace import Status, StatusCode, TracerProvider
-from opentelemetry_resourcedetector_docker import DockerResourceDetector
-from opentelemetry_resourcedetector_process import ProcessResourceDetector
+from opentelemetry.trace import Status, StatusCode
+from opentelemetry_container_distro import (
+    OpenTelemetryContainerConfigurator,
+    OpenTelemetryContainerDistro,
+)
 
-from pytest_opentelemetry.resource import CodebaseResourceDetector
+from .resource import CodebaseResourceDetector
 
 try:
     from xdist.workermanage import WorkerController  # pylint: disable=unused-import
@@ -30,19 +28,6 @@ tracer = trace.get_tracer('pytest-opentelemetry')
 class OpenTelemetryPlugin:
     """A pytest plugin which produces OpenTelemetry spans around test sessions and
     individual test runs."""
-
-    @staticmethod
-    def _initialize_trace_provider(resource: Resource, export: bool) -> TracerProvider:
-        provider = trace.get_tracer_provider()
-
-        if not isinstance(provider, trace_sdk.TracerProvider):
-            provider = trace_sdk.TracerProvider(resource=resource)
-            trace.set_tracer_provider(provider)
-
-        if export:  # pragma: no cover (This can't be tested both ways in one process)
-            provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
-
-        return provider
 
     @staticmethod
     def get_trace_parent(config: Config) -> Optional[Context]:
@@ -59,16 +44,13 @@ class OpenTelemetryPlugin:
         self.trace_parent = self.get_trace_parent(config)
         self.worker_id = getattr(config, 'workerinput', {}).get('workerid')
 
-        self.provider = self._initialize_trace_provider(
-            resource=get_aggregated_resources(
-                [
-                    CodebaseResourceDetector(),
-                    DockerResourceDetector(),
-                    ProcessResourceDetector(),
-                ]
-            ),
-            export=config.getoption('--export-traces'),
-        )
+        # This can't be tested both ways in one process
+        if config.getoption('--export-traces'):  # pragma: no cover
+            OpenTelemetryContainerDistro().configure()
+
+        configurator = OpenTelemetryContainerConfigurator()
+        configurator.resource_detectors.append(CodebaseResourceDetector())
+        configurator.configure()
 
     def pytest_sessionstart(self, session: Session) -> None:
         session_name = f'test worker {self.worker_id}' if self.worker_id else 'test run'
