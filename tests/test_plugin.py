45ef580aae71064bde9b59eccf7b289d1d229908
Chris Guidry <chris@theguidrys.us>
2022-09-18 07:16:54 -0400

    Test for package versioning.

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,14 @@
 from _pytest.config import Config
+from packaging.version import Version
 
 import pytest_opentelemetry.plugin
+from pytest_opentelemetry import __version__
+
+
+def test_version_is_sane() -> None:
+    assert __version__
+    assert Version(__version__)
+    assert Version(__version__) > Version('0.4.0')  # before we introduced __version__
 
 
 def test_plugin_is_loaded(pytestconfig: Config) -> None:
