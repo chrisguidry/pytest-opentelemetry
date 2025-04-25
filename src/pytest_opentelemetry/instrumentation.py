109224aff3890968c7e46187688a7962d614a4c4
Martin Pilros <58504806+martin-pil@users.noreply.github.com>
2025-04-25 14:29:04 +0200

    Use pytest functionality to lookup codebase name (#35)
    Co-authored-by: Chris Guidry <chris@theguidrys.us>

--- src/pytest_opentelemetry/instrumentation.py
+++ src/pytest_opentelemetry/instrumentation.py
@@ -59,7 +59,7 @@ class PerTestOpenTelemetryPlugin:
             OpenTelemetryContainerDistro().configure()
 
         configurator = OpenTelemetryContainerConfigurator()
-        configurator.resource_detectors.append(CodebaseResourceDetector())
+        configurator.resource_detectors.append(CodebaseResourceDetector(config))
         configurator.resource_detectors.append(OTELResourceDetector())
         configurator.configure()
 
